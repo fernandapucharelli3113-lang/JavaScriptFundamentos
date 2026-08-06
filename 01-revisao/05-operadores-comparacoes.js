4b583846d648f17020538b934420e0f0917d4fa0
FERNANDA PUCHARELLI SANTOS <26171647@edu119.local>
2026-08-06 10:24:38 -0300

    Salvando Revisão

---
+++ 01-revisao/05-operadores-comparacoes.js
@@ -0,0 +1,10 @@
+console.log(10 > 5);    //true
+console.log(10 < 5);    //false
+console.log(10 >= 5);    //true
+console.log(10 <= 5);    //false
+console.log(10 === 10);    //true
+console.log(10 === "10");    //false
+console.log(10 !== 5);    //true
+
+
+// prefira === 0 1==, pois também compare o tipo de dado.
---
+++ 01-revisao/06-template-string.js
@@ -0,0 +1,5 @@
+const nome = "Fernanda";
+const nota = 8.5;
+
+console.log("O aluno " + nome + " obteve nota " + nota + ".");
+console.log(`O aluno ${nome} obteve nota ${nota}.`);
