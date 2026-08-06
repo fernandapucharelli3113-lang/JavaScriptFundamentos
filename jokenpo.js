f9a6078d378fcbe39e645ee380902254a1ba8655
FERNANDA PUCHARELLI SANTOS <26171647@edu119.local>
2026-08-06 16:57:01 -0300

    Salvando alterações

---
+++ jokenpo.js
@@ -0,0 +1,10 @@
+const jogador1 = "pedra";
+const jogador2 = "papel";
+
+
+if (jogador1 === jogador2){
+    console.log("Empate!");
+}else(jogador1 < jogador2)
+{
+    console.log(`O jogador 2 ${jogador2}, ganhou!`);
+}
